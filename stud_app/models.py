705f3437b3b08920622b2d701ca8d554df71119f
Sarisha.tk <sarishakrishna58@gmail.com>
2025-10-23 13:30:36 +0530

    student management

---
+++ stud_app/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+
+class StudentModel(models.Model):
+
+    name = models.CharField(max_length=20)
+
+    email = models.CharField(max_length=20)
+
+    course = models.CharField(max_length=20)
+
+    age = models.IntegerField()
+
+
---
+++ stud_app/views.py
@@ -0,0 +1,84 @@
+from django.shortcuts import render
+from stud_app.models import StudentModel
+
+from django.views.generic import View
+
+# Create your views here.
+
+# create student 
+
+class CreateStudView(View):
+
+    def get(self,request):
+
+        return render(request,"create_stud.html")
+    
+    def post(self,request):
+
+        print(request.POST)
+
+        StudentModel.objects.create(name = request.POST.get('username'),
+                                    email = request.POST.get('email'),
+                                     course = request.POST.get('course'),
+                                     age = request.POST.get('age')
+                                    )
+
+        return render(request,"create_stud.html")
+
+
+# read student
+
+class ReadView(View):
+
+    def get(self,request):
+
+        data = StudentModel.objects.all()
+
+        return render(request,"list_stud.html",{'data':data})
+    
+
+# update view
+
+class UpdateView(View):
+
+    def get(self,request):
+
+        data = StudentModel.objects.get(id = 3)
+
+        return render(request,"update.html",{'data':data})
+    
+    def post(self,request):
+
+            data = StudentModel.objects.get(id = 3)
+
+            print(request.POST)
+
+            data.name = request.POST.get('username')
+
+            data.email =  request.POST.get('email')
+
+            data.course = request.POST.get('course')
+
+            data.age =  request.POST.get('age')
+
+            data.save()
+
+            return render(request,"update.html")
+    
+
+
+# deleteview
+
+class DeleteView(View):
+     
+     def get(self,request):
+          
+          data = StudentModel.objects.get(id=1)
+
+          data.delete()
+
+          return render(request,"create_stud.html")
+          
+
+
+         
